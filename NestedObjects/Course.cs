4b885e07b0c85fe415c6f25f1cd633b8202c0f6f
davidrodjen <davidrodriguezjenkins@gmail.com>
2020-01-15 14:17:24 -0800

    Updated documentation for course class

--- NestedObjects/Course.cs
+++ NestedObjects/Course.cs
@@ -8,12 +8,26 @@ namespace NestedObjects
 {
     public class Course
     {
+        /// <summary>
+        /// The title of the course ex. Programming Fundamentals
+        /// </summary>
         public string CourseTitle { get; set; }
 
+        /// <summary>
+        /// The text description of the course.
+        /// HTML elements are not allowed
+        /// </summary>
         public string Description { get; set; }
 
+        /// <summary>
+        /// The unique course id assigned to the course
+        /// </summary>
         public int CourseId { get; set; }
 
+        /// <summary>
+        /// The number of college credits earned
+        /// for completing the class
+        /// </summary>
         public byte Credits { get; set; }
     }
 }
